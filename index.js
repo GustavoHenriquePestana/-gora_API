3bc80fb859b6525ba75086490591a284136dae73
marcioA <zamar_pinheiro@hotmail.com>
2023-04-29 20:58:33 -0300

    up

--- index.js
+++ index.js
@@ -36,11 +36,11 @@ app.use('/rented', rentedRoutes)
 //rota author
 
 const authorRoutes = require('./routes/authorRoutes')
-app.use('/author', rentedRoutes)
+app.use('/author', authorRoutes)
 
 //rotas iniciais
-app.get('/', (req, res)=>{
-    res.json({message: 'Oi Express!'})
+app.get('/', (req, res) => {
+    res.json({ message: 'Oi Express!' })
 })
 
 //entregar porta
@@ -51,8 +51,8 @@ const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD)
 mongoose.connect(
     `mongodb+srv://${DB_USER}:${DB_PASSWORD}@agoracluster.z8ih2r0.mongodb.net/?retryWrites=true&w=majority`
 )
-.then(()=>{
-    console.log('Conectamos ao MongoDB!')
-    app.listen(3000)
-})
-.catch((err)=> console.log(err))
+    .then(() => {
+        console.log('Conectamos ao MongoDB!')
+        app.listen(3000)
+    })
+    .catch((err) => console.log(err))
